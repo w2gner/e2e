7a9d2dcc2f834f726887d3b6ff824d606fda675b
Wagner Ghedin <wagnerghedin10@gmail.com>
2021-05-03 20:43:10 -0300

    Adicionar arquivos de projeto.

---
+++ Tests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using RestSharp;
+using System;
+
+namespace Tests
+{
+    class ListOfTests : jsonplaceholder
+    {
+        [Test]
+        public void GetUsersList()
+        {
+            IRestResponse response = GetUsersRequest();
+            Assert.AreEqual("OK", response.StatusCode.ToString());
+            Assert.IsTrue(response.Content.Contains("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
+        }
+
+        [Test]
+        public void CreateNewUser()
+        {
+            IRestResponse response = CreateUserRequest();
+            Assert.AreEqual("Created", response.StatusCode.ToString());
+            Assert.IsTrue(response.Content.Contains("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
+        }
+
+        [Test]
+        public void DeleteUser()
+        {
+            IRestResponse response = DeleteUserRequest();
+            Assert.AreEqual("OK", response.StatusCode.ToString());
+        }
+
+        [Test]
+        public void EditUser()
+        {
+            IRestResponse response = CreateUserRequest();
+            Assert.AreEqual("Created", response.StatusCode.ToString());
+            Assert.IsTrue(response.Content.Contains("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
+        }
+
+
+        [Test]
+        public void CheckPageTitle()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://jsonplaceholder.typicode.com/";
+            IWebElement Title = driver.FindElement(By.CssSelector("[class='text-6xl bold']"));
+
+            Assert.True(Title.Displayed);
+            driver.Close();
+        }
+
+       [Test]
+        public void CheckNavigation()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://jsonplaceholder.typicode.com/";
+
+            IWebElement runScriptBtn = driver.FindElement(By.CssSelector("[id='run-button']"));
+            IWebElement guideBtn = driver.FindElement(By.CssSelector("[class='mr-4 no-underline']"));
+            IWebElement result = driver.FindElement(By.CssSelector("[id='result']"));
+            
+
+            runScriptBtn.Click();
+            Assert.True(result.Displayed);
+            guideBtn.Click();
+            driver.Close();
+        }
+    }
+}
---
+++ jsonplaceholder.cs
@@ -0,0 +1,48 @@
+﻿using RestSharp;
+using System;
+
+//This is not really a page object
+public class jsonplaceholder
+{
+    public static IRestResponse GetUsersRequest()
+    {
+        var client = new RestClient("https://jsonplaceholder.typicode.com/posts/1");
+        var request = new RestRequest(Method.GET);
+        IRestResponse response = client.Execute(request);
+        return response;
+    }
+
+    public static IRestResponse CreateUserRequest()
+    {
+        var client = new RestClient("https://jsonplaceholder.typicode.com/posts");
+        var request = new RestRequest(Method.POST);
+        request.AddHeader("Content-Type", "application/json");
+        request.AddJsonBody("{\n  \"userId\": 1,\n  \"id\": 1,\n  \"title\": \"sunt aut facere repellat p" +
+            "rovident occaecati excepturi optio reprehenderit\",\n  \"body\": \"quia et suscipit\\nsuscipit " +
+            "recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum " +
+            "est autem sunt rem eveniet architecto\"\n}");
+        IRestResponse response = client.Execute(request);
+        return response;
+    }
+
+    public static IRestResponse DeleteUserRequest()
+    {
+        var client = new RestClient("https://jsonplaceholder.typicode.com/posts/1");
+        var request = new RestRequest(Method.DELETE);
+        IRestResponse response = client.Execute(request);
+        return response;
+    }
+
+    public static IRestResponse EditUserRequest()
+    {
+        var client = new RestClient("https://jsonplaceholder.typicode.com/posts/1");
+        var request = new RestRequest(Method.PUT);
+        request.AddHeader("Content-Type", "application/json");
+        request.AddJsonBody("{\n  \"userId\": 1,\n  \"id\": 1,\n  \"title\": \"sunt aut facere repellat p" +
+            "rovident occaecati excepturi optio reprehenderit\",\n  \"body\": \"quia et suscipit\\nsuscipit " +
+            "recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum " +
+            "est autem sunt rem eveniet architecto\"\n}");
+        IRestResponse response = client.Execute(request);
+        return response;
+    }
+}
